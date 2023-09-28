af6f2dc3238be540db05b1c35a7295afda6c83e3
Ray7808 <yuwang7808@gmail.com>
2023-09-28 20:42:04 +0800

    feat: add hyperparameters

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ from utils import (
 from tqdm import tqdm
 import os
 import numpy as np
+
+# Hyperparameters etc.
+LEARNING_RATE = 1e-3  # 1e-4
+DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+# print(torch.cuda.device_count())
+# print(torch.cuda.get_device_name())
+BATCH_SIZE = 10
+NUM_EPOCHS = 1500
+IMAGE_HEIGHT = 200
+IMAGE_WIDTH = 200
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images/"
+TRAIN_MASK_DIR = "data/train_masks/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
+TEST_IMG_DIR = "data/testing/test_images/"
+TEST_MASK_DIR = "data/testing/test_masks/"
+PIN_MEMORY = True
+NUM_WORKERS = 4
+Initial_train_loss = 10000000 # Lucky number
+Initial_val_loss = 10000000 # Lucky number
